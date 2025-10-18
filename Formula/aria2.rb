7987e414b51a77e5c1afbac9469d1a6db4599459
John Faust <45057319+slickag@users.noreply.github.com>
2025-10-18 12:28:16 +0000

    aria2 1.37.0 (new formula)

---
+++ Formula/aria2.rb
@@ -0,0 +1,75 @@
+class Aria2 < Formula
+  desc "Download with resuming and segmented downloading"
+  homepage "https://aria2.github.io/"
+  url "https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0.tar.xz"
+  sha256 "60a420ad7085eb616cb6e2bdf0a7206d68ff3d37fb5a956dc44242eb2f79b66b"
+  license "GPL-2.0-or-later"
+
+  head do
+    url "https://github.com/aria2/aria2.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkgconf" => :build
+
+  depends_on "c-ares"
+  depends_on "libssh2"
+  depends_on "sqlite"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gettext"
+  end
+
+  on_linux do
+    depends_on "openssl"
+  end
+
+  def install
+    ENV.cxx11
+    ENV.clang
+
+    if build.head?
+      ENV.append_to_cflags "-march=native -O3 -pipe -flto=auto"
+
+      system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    end
+
+    args = %w[
+      --disable-silent-rules
+      --disable-nls
+      --enable-metalink
+      --enable-bittorrent
+      --with-libcares
+      --with-libssh2
+      --with-libxml2
+      --with-libz
+      --without-gnutls
+      --without-libgcrypt
+      --without-libgmp
+      --without-libnettle
+    ]
+    if OS.mac?
+      args << "--with-appletls"
+      args << "--without-openssl"
+    else
+      args << "--without-appletls"
+      args << "--with-openssl"
+    end
+
+    system "./configure", *args, *std_configure_args
+    system "make", "install"
+
+    bash_completion.install "doc/bash_completion/aria2c"
+  end
+
+  test do
+    system bin/"aria2c", "https://brew.sh/"
+    assert_path_exists testpath/"index.html", "Failed to create index.html!"
+  end
+end
