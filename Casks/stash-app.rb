7f04735ef92c951fffa1c6e44a69058e9b0edb33
John Faust <45057319+slickag@users.noreply.github.com>
2025-11-08 15:19:49 +0000

    stash-app 0.29.3
    Closes #9.
    
    Signed-off-by: John Faust <45057319+slickag@users.noreply.github.com>

--- Casks/stash-app.rb
+++ Casks/stash-app.rb
@@ -1,6 +1,6 @@
 cask "stash-app" do
-  version "0.29.1"
-  sha256 "c2244a3caad8875c8c1c85bb05a30a490b0f679bce3586ae484d24bfdf95e148"
+  version "0.29.3"
+  sha256 "a010afe8dfa87fedaa204211c1e48432700c37a3d2e87c81a1b5e80777012898"
 
   url "https://github.com/stashapp/stash/releases/download/v#{version}/Stash.app.zip",
       verified: "github.com/stashapp/stash/"
