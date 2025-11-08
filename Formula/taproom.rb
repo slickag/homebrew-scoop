441dfab04bd34bb51712c137264bf9d634a723d7
John Faust <45057319+slickag@users.noreply.github.com>
2025-11-08 15:16:58 +0000

    taproom: update 0.4.5 bottle.

--- Formula/taproom.rb
+++ Formula/taproom.rb
@@ -8,8 +8,13 @@ class Taproom < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/slickag/scoop"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "425dc152f8823b430379c21f0d4244e363ff5de46586545ff0794df0f0113d7e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9db7fe6c2141f2183e579c7400b220d9e6fcdfeb8fe580ea69aabc9ed8cb30e9"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9e0ad2df306742adc0e2deedd4119560e616008a56954f26efe04eef4b52d30f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2484e19ef16be970bf9693ca70fd282b33b07c54bbb55c89bae37eb8c0bc2422"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5aea8c9009e1472abd538f46310a77e378b443753bd32c8588f9cdf01be8fb20"
+    sha256 cellar: :any_skip_relocation, sequoia:       "82356441cfe58c16ac9c0d41e6311260fe1fc33e16ad25fae6a403e9fe3b0289"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9d72ceec9040957c4b8233b5cd02530c2cd96a947ecebf56d2a9a8148d4ca9ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae6595a4bb1a3cd9fb7aea4968f82395d4d5b85b1bb7018d8e667abdebb609cb"
   end
 
   depends_on "go" => :build
